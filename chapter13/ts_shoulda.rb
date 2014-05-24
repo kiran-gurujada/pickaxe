8d4470bcd1e8d540db2a1969c3b348bcddcf37dc
Sam Philip <seivadmas@gmail.com>
2014-05-24 16:39:55 -0400

    chapter 13 complete

---
+++ chapter13/ts_shoulda.rb
@@ -0,0 +1,31 @@
+require 'test/unit'
+require 'shoulda'
+require_relative 'tennis_scorer.rb'
+
+class TennisScorerTest < Test::Unit::TestCase
+
+  def assert_score(target)
+    assert_equal(target, @ts.score)
+  end
+
+  context "Tennis scores" do
+    setup do
+      @ts = TennisScorer.new
+    end
+
+    should "start with a score of 0-0" do
+      assert_score("0-0")
+    end
+
+    should "be 15-0 if the server wins a point" do
+      @ts.give_point_to(:server)
+      assert_score("15-0")
+    end
+
+    should "be 0-15 if the receiver wins a point" do
+      @ts.give_point_to(:receiver)
+      @ts.give_point_to(:server)
+      assert_score('15-15')
+    end
+  end
+end
